a6f3fa4f827846a6b2fd77eb4a7c301ac4aff990
corganfuzz <gruizcavero@uh.edu>
2020-06-05 12:57:46 -0500

    finishing writting tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently</h1>
-      <button data-test="increment-button">Increment counter</button>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    counter: 0,
+  };
 
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <button
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increment counter
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
